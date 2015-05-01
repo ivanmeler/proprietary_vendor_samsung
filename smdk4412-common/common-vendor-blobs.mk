0a24b622051f28fbc187c0fe977c6cb87d38c976
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2015-05-01 00:00:00 +0000

    NL1: WiFi blobs

--- smdk4412-common/common-vendor-blobs.mk
+++ smdk4412-common/common-vendor-blobs.mk
@@ -21,7 +21,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/bin/bcm4334.hcd:system/bin/bcm4334.hcd \
     $(LOCAL_PATH)/proprietary/system/bin/bcm4334_murata.hcd:system/bin/bcm4334_murata.hcd \
     $(LOCAL_PATH)/proprietary/system/bin/bcm4334_semco.hcd:system/bin/bcm4334_semco.hcd \
-    $(LOCAL_PATH)/proprietary/system/bin/bcm4334_semcosh.hcd:system/bin/bcm4334_semcosh.hcd \
     $(LOCAL_PATH)/proprietary/system/bin/gps.cer:system/bin/gps.cer
 
 PRODUCT_COPY_FILES += \
